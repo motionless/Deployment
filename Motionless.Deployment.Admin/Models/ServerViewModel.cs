fb9c2675f51b7ddbf543beef2dd80a1881af3a4d
Henry Gallert <henry.gallert@motionless.de>
2013-06-11 08:08:30 +0200

    small changes

--- Motionless.Deployment.Admin/Models/ServerViewModel.cs
+++ Motionless.Deployment.Admin/Models/ServerViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 using Motionless.Deployment.Contracts.Data.Model;
 
 namespace Motionless.Deployment.Admin.Models
@@ -14,6 +15,25 @@ namespace Motionless.Deployment.Admin.Models
 		public DateTime UpdatedAt { get; set; }
 		public string Name { get; set; }
 		public ISet<IServerRole> ServerRoles { get; set; }
+		
 		public ISet<IEnvironment> Environments { get; set; }
+
+		public List<long> SelectedEnvironmentIds { get; set; }
+
+		
+		public ISet<IEnvironment> SelectableEnvironments { get; set; }
+		public MultiSelectList EnvironmentsSelectList
+		{
+			get
+			{
+				var listItems = SelectableEnvironments.Select(p => new SelectListItem() { Text = p.Name, Value = p.Id.ToString() }).ToList();
+
+				return new SelectList(listItems,
+										"Value",
+										"Text",
+										/*Product != null ? Product.Id : 0*/0);
+			}
+		}
+
 	}
 }
