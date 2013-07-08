a7b47e2ada1cb2780d6efda8a0212617ce89da1f
Henry Gallert <henry.gallert@motionless.de>
2013-07-08 08:09:35 +0200

    change discovery worker change test config temporary

--- Motionless.Deployment.Service/PackageDiscoveryWorker.cs
+++ Motionless.Deployment.Service/PackageDiscoveryWorker.cs
@@ -25,6 +25,7 @@ namespace Motionless.Deployment.Service
 		{
 			PackageConfigurationService = new PackageConfigurationService();
 			ProductService = new ProductService();
+			PackageService = new PackageService();
 		}
 
 		/// <summary>
@@ -32,62 +33,90 @@ namespace Motionless.Deployment.Service
 		/// </summary>
 		public void ProcessPackageConfigurations()
 		{
-			
+			// get all products that are configured
 			IEnumerable<IProduct> products = ProductService.GetAll();
+			// for each product try to find new packages
 			foreach (IProduct product in products)
 			{
+				IPackageConfiguration configuration = null;
+
 				if (product.PackageConfigurations != null)
 				{
-					var configurations = product.PackageConfigurations.Where(config => config.IsActive).ToList();
-					foreach (IPackageConfiguration packageConfiguration in configurations)
+					configuration = product.PackageConfigurations
+					                       .Where(config => config.IsActive)
+					                       .OrderByDescending(config => config.UpdatedAt)
+					                       .FirstOrDefault();
+				}
+
+				Version lastPackageVersion = null;
+				if (product.Packages != null)
+				{
+					var packageWithHighestVersion = product.Packages.OrderBy(package => package.Version).LastOrDefault();
+					if (packageWithHighestVersion != null)
 					{
-						var lastPackageForConfiguration = packageConfiguration.Packages.OrderBy(package => package.CreatedAt).LastOrDefault();
-						if (lastPackageForConfiguration != null)
+						lastPackageVersion = packageWithHighestVersion.Version;
+					}
+				}
+
+				if (configuration != null)
+				{
+					if (configuration.SearchPath != null)
+					{
+						string lastFileName = null;
+
+						if (lastPackageVersion != null)
 						{
-							var version = lastPackageForConfiguration.Version;
-							var name = lastPackageForConfiguration.Name;
-
-							if (packageConfiguration.SearchPath != null)
+							lastFileName = string.Format("{0}_{1}.{2}.{3}.zip", 
+							                             configuration.Name, 
+							                             lastPackageVersion.Major,
+							                             lastPackageVersion.Minor, 
+							                             lastPackageVersion.Build);
+						}
+						
+						if (Directory.Exists(configuration.SearchPath))
+						{
+							foreach (string packageUrl in Directory.EnumerateFiles(configuration.SearchPath, configuration.SearchPattern))
 							{
-								var lastFileName = string.Format("{0}_{1}.{2}.{3}.zip", name, version.Major, version.Minor, version.Build);
-								foreach (string file in Directory.EnumerateFiles(packageConfiguration.SearchPath, packageConfiguration.SearchPattern))
+								var fileName = new FileInfo(packageUrl).Name;
+
+								if (String.Compare(fileName, lastFileName, StringComparison.InvariantCultureIgnoreCase) > 0)
 								{
-									if (String.Compare(file, lastFileName, StringComparison.InvariantCultureIgnoreCase) > 0)
-									{	
-										//create package for this configuration
-										IPackage package = PackageService.CreateInstance();
-										Match matches = FilenamePattern.Match(file);
+									//create package for this configuration
+									IPackage package = PackageService.CreateInstance();
+									Match matches = FilenamePattern.Match(fileName);
 
-										if (matches.Success)
+									if (matches.Success)
+									{
+										package.Name = matches.Groups[1].Value;
+
+										int major;
+										if (int.TryParse(matches.Groups[2].Value, out major))
 										{
-											package.Name = matches.Groups[1].Value;
-
-											int major;
-											if (int.TryParse(matches.Groups[2].Value, out major))
+											int minor;
+											if (int.TryParse(matches.Groups[3].Value, out minor))
 											{
-												int minor;
-												if (int.TryParse(matches.Groups[3].Value, out minor))
+												int build;
+												if (int.TryParse(matches.Groups[4].Value, out build))
 												{
-													int build;
-													if (int.TryParse(matches.Groups[4].Value, out build))
-													{
-														package.Version = new Version(major,minor,build);
-													}
+													package.Version = new Version(major, minor, build);
 												}
 											}
 										}
-										package.Product = product;
-										package.Websites = packageConfiguration.Websites;
-										package.SetupSteps = packageConfiguration.SetupSteps;
-										package.PackageUrl = file;
 									}
-								}
 
+									package.Name = fileName;
+									package.Product = product;
+									package.Websites = configuration.Websites;
+									package.SetupSteps = configuration.SetupSteps;
+									package.PackageUrl = packageUrl;
+								}
 							}
 						}
+
 					}
 				}
 			}
 		}
 	}
 }
+
